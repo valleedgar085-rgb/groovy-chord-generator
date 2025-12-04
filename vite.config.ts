7409417e47b26f76a8ed00648aee02775d3cf614
valleedgar085-rgb <valleedgar085@gmail.com>
2025-12-03 20:35:35 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,4 +19,4 @@ export default defineConfig({
     port: 5173,
     open: true,
   },
-})
+});
